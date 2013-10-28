4f345c7c0d14f91a0a7354dacbe0b2b1da5f1941
samhunter <shunter@gmail.com>
2013-10-28 15:07:06 -0700

    Better docs for extract_unmapped_reads.py

--- extract_unmapped_reads.py
+++ extract_unmapped_reads.py
@@ -1,11 +1,18 @@
 #!/usr/bin/env python
 
 '''
-Iterate over a SAM or SAM.gz file, take everything where the 3rd and
+Extract reads which aren't mapped from a SAM or SAM.gz file.
+Behavior for PE:
+    -Write out PE only if both do not map (if either of the pair maps, neither is retained)
+    -The logic on the FLAG bit looks like this:
+    write if: (flag & 0x1 == 1) and (flag & 0x4 == 1) and (flag & 0x8 == 1)
+Behavior for SE:
+    -Write out SE if they don't map
+    -The logic on the FLAG bit looks like this:
+    write if (flag & 0x1 == 0) and (flag & 0x4 == 1)
+
+Iterate over a SAM or SAM.gz file. take everything where the 3rd and
 4th flag bit are set to 1 and write reads out to files.
-The SAM definition requires that pairs appear consequitively for PE reads,
-so the trick is to read in two lines, compare them to make sure
-they are paired, and then output to .fastq.
 
     0x1 template having multiple segments in sequencing
     0x2 each segment properly aligned according to the aligner
@@ -18,10 +25,20 @@ they are paired, and then output to .fastq.
     0x100 secondary alignment
     0x200 not passing quality controls
     0x400 PCR or optical duplicate
+
+TODO:
+    1) Add support for retaining both reads if one of a pair don't map but the other does
+    2) Add support for retaining the pair (or SE) if a read maps with low mapq
+
+Note:
+    It is necessary to double check that both pairs of the PE read really exist in the SAM
+    file just in case it somehow gets disordered. This is taken care of by keeping the PE
+    reads in a set of dictionaries and then deleting them once the pair is written.
+    In the case where a read is somehow labeled as paired, but the pair doesn't exist, the
+    read is NOT written.
 '''
 import sys
 import os
-#from collections import OrderedDict
 from optparse import OptionParser  # http://docs.python.org/library/optparse.html
 import gzip
 
@@ -33,7 +50,7 @@ parser.add_option('-u', '--uncompressed', help="leave output files uncompressed"
 
 (options,  args) = parser.parse_args()  # uncomment this line for command line support
 
-if len(args) < 1:
+if len(args) < 2:
     parser.print_help()
     sys.exit()
 
@@ -75,31 +92,47 @@ def writeread(ID, r1, r2):
 
 i = 0
 PE_written = 0
+SE_written = 0
 for line in insam:
-    i += 1
-    if i % 100000 == 0:
-        print "Record %s" % i
-        print "\t Unwritten: PE1 reads ", len(PE1), "PE2 reads: ", len(PE2)
-        print "\t Written: %s" % PE_written
-    line2 = line.strip().split()
-    if line[0] != "@" and len(line2) > 2:
+    if i % 100000 == 0 and i > 0:
+        print "Records processed: %s, PE_written: %s, SE_written: %s" % (i, PE_written, SE_written)
+        #print "\t Unwritten: PE1 reads ", len(PE1), "PE2 reads: ", len(PE2)
+        #print "\t Written: %s" % PE_written
+    #if line[0] != "@" and len(line2) > 2:
+    #    flag = int(line2[1])
+    #if line[0] != "@":
+    #Comment/header lines start with @
+    if line[0] != "@" and len(line.strip().split()) > 2:
+        i += 1
+        line2 = line.strip().split()
         flag = int(line2[1])
-    if line[0] != "@":
+
+        #Handle SE:
+        # unapped SE reads have 0x1 set to 0, and 0x4 (third bit) set to 1
+        if (flag & 0x1 == 0) and (flag & 0x4):
+            ID = line2[0].split("#")[0]
+            outSE.write("@" + ID + '\n')
+            outSE.write(line2[9] + '\n')
+            outSE.write('+\n' + line2[10] + '\n')
+            SE_written += 1
+            continue
+
+        #Handle PE:
         #logic:  0x1 = multiple segments in sequencing,   0x4 = segment unmapped,  0x8 = next segment unmapped
         # which means (is paired, and this segment is unmapped and the next segment is unmapped)
-        if (flag & 0x1 != 0 and flag & 0x4 != 0 and flag & 0x8 != 0):
-            #if line2[0][-3:] == '0/1':
-            if flag & 0x40 != 0:  # is this PE1 (first segment in template)
+        #if ((flag & 0x1) == 1 and (flag & 0x4) != 0 and flag & 0x8 != 0):
+        if ((flag & 0x1) and (flag & 0x4) and (flag & 0x8)):
+            if (flag & 0x40):  # is this PE1 (first segment in template)
                 #PE1 read, check that PE2 is in dict and write out
                 ID = line2[0].split("#")[0]
-                r1 = [line2[9], line2[10]]
+                r1 = [line2[9], line2[10]]  # sequence + qual
                 if ID in PE2:
                     writeread(ID, r1, PE2[ID])
                     del PE2[ID]
                     PE_written += 1
                 else:
                     PE1[ID] = r1
-            elif flag & 0x80 != 0:  # is this PE2 (last segment in template)
+            elif (flag & 0x80):  # is this PE2 (last segment in template)
                 #PE2 read, check that PE1 is in dict and write out
                 ID = line2[0].split("#")[0]
                 r2 = [line2[9], line2[10]]
@@ -109,24 +142,21 @@ for line in insam:
                     PE_written += 1
                 else:
                     PE2[ID] = r2
-        elif flag & 0x4 != 0:  # unapped SE reads have a flag value of 4 (third bit set)
-            outSE.write("@" + line2[0] + '\n')
-            outSE.write(line2[9] + '\n')
-            outSE.write('+\n' + line2[10] + '\n')
 
+print "Records processed: %s, PE_written: %s, SE_written: %s" % (i, PE_written, SE_written)
 ## Finally go through PE1 and PE2, write out any SE reads that might exist:
-print "Checking for unmatched, paired reads and writing as SE"
-print "PE1 reads: ", len(PE1)
-print "PE2 reads: ", len(PE2)
+# print "Checking for unmatched, paired reads and writing as SE"
+# print "PE1 reads: ", len(PE1)
+# print "PE2 reads: ", len(PE2)
 
-for k in PE1.keys():
-    outSE.write("@" + k + '#_1\n')
-    outSE.write(PE1[k][0] + '\n')
-    outSE.write('+\n' + PE1[k][1] + '\n')
-for k in PE2.keys():
-    outSE.write("@" + k + '#_2\n')
-    outSE.write(PE2[k][0] + '\n')
-    outSE.write('+\n' + PE2[k][1] + '\n')
+# for k in PE1.keys():
+#     outSE.write("@" + k + '#_1\n')
+#     outSE.write(PE1[k][0] + '\n')
+#     outSE.write('+\n' + PE1[k][1] + '\n')
+# for k in PE2.keys():
+#     outSE.write("@" + k + '#_2\n')
+#     outSE.write(PE2[k][0] + '\n')
+#     outSE.write('+\n' + PE2[k][1] + '\n')
 
 outPE1.close()
 outPE2.close()
